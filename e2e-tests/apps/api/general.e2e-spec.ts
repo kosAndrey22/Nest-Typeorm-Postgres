2dd52a8fa1ad27745fb71c5c806818fb080a296d
kosAndre22 <a.kosobuckiy@pixelplex.io>
2022-02-16 18:44:21 +0300

    Add general testing and edit configs

---
+++ e2e-tests/apps/api/general.e2e-spec.ts
@@ -0,0 +1,31 @@
+import request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { GeneralModule } from '../../../apps/api/src/general/general.module';
+
+
+describe('General', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [GeneralModule],
+    })
+      .compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  it(`/GET general`, () => {
+    return request(app.getHttpServer())
+      .get(`/general`)
+      .expect(200)
+      .expect({});
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+});
